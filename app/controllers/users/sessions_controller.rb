8f656cdedb7ee42f60562e836ddcec5cad329cad
amitesh <amiteshs@chetu.com>
2018-03-06 22:52:53 +0530

    Changes for fullcal

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,16 +26,16 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
 
-  def create
-    binding.pry
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url
-  end
+  # def create
+  #   binding.pry
+  #   user = User.from_omniauth(env["omniauth.auth"])
+  #   session[:user_id] = user.id
+  #   redirect_to root_url
+  # end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_url
+  # end
 
 end
